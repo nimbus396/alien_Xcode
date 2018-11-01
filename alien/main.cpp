22a7747296e8290c0c249f8017a2b42d41eea7f8
James Johnson <nimbus396@hotmail.com>
2018-11-01 18:14:22 -0500

    Initial Commit

---
+++ alien/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alien
+//
+//  Created by James Johnson on 11/1/18.
+//  Copyright © 2018 James Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
